c95dc6449f47233b74c4ec08c1f2c040450f58e2
Ahmad El-Bobou <ahmad.elbobou@gmail.com>
2022-06-18 17:39:04 -0500

    Created document upload form.

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,39 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
+import { Box, Button, Center, Heading, Spacer } from "native-base";
+import { useDocumentUpload } from "../hooks/useDocumentUpload";
+import { ActivityIndicator } from "react-native-paper";
+
 type Props = {};
 
 const HomeScreen = (props: Props) => {
+    const {
+        openDocumentPicker,
+        isLoading,
+        uploadDocument,
+        documentSelected,
+        documentResult,
+    } = useDocumentUpload();
     return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
+        <Center flex={1}>
+            <Box height={"50%"}>
+                {isLoading && <ActivityIndicator color="grey" size="large" />}
+
+                {!documentSelected && (
+                    <Button onPress={openDocumentPicker}>Pick document</Button>
+                )}
+                {documentSelected && documentResult?.type == "success" && (
+                    <>
+                        <Heading>{documentResult.name}</Heading>
+                        <Spacer my={1} />
+                        <Button onPress={uploadDocument}>
+                            Upload document
+                        </Button>
+                    </>
+                )}
+            </Box>
+        </Center>
     );
 };
 
