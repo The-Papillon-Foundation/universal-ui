18635bbfe5a76ab6c9065a414b45ada715a79b62
Ahmad El-Bobou <ahmad.elbobou@gmail.com>
2022-08-28 14:13:52 -0500

    SFBETA-17: #time 6hr #comment added id ocr to create user flow.

--- hooks/useIdUpload.ts
+++ hooks/useIdUpload.ts
@@ -1,5 +1,12 @@
+import { Camera, CameraCapturedPicture } from "expo-camera";
 import * as DocumentPicker from "expo-document-picker";
-import React, { useContext, useState } from "react";
+import React, {
+    MutableRefObject,
+    useContext,
+    useEffect,
+    useRef,
+    useState,
+} from "react";
 import { url } from "../constants/Urls";
 import { GlobalContext } from "../contexts/GlobalContext";
 
@@ -25,6 +32,9 @@ export const useIdUpload = () => {
         useState<DocumentPicker.DocumentResult | null>(null);
     const [isLoading, setIsLoading] = useState(false);
     const [isUploaded, setIsUploaded] = useState(false);
+    const [cameraOpen, setCameraOpen] = useState(false);
+    const cameraRef = useRef<Camera | null>(null);
+    const [photoInfo, setPhotoInfo] = useState<CameraCapturedPicture | null>();
 
     // user data
     const [firstName, setFirstName] = useState("");
@@ -60,46 +70,73 @@ export const useIdUpload = () => {
         setIsLoading(false);
     };
 
-    const uploadDocument = async () => {
-        if (documentResult == null)
-            return alert("must select document to upload");
-        if (
-            documentResult.type == "success" &&
-            documentResult.file != undefined
-        ) {
-            // const data = new FormData();
-            // data.append("image", documentResult.file);
-            const body = {
-                image: await blobToBase64(documentResult.file),
-            };
-            setIsLoading(true);
-            try {
-                const res = await fetch(`${url}/users/${userId}/id`, {
-                    method: "POST",
-                    body: JSON.stringify(body),
-                });
-                if (res.status == 201) {
-                    const resjson = await res.json();
-                    const userInfo: { [key: string]: string } =
-                        resjson.userInfo;
-
-                    setFirstName(userInfo.FIRST_NAME);
-                    setMiddleName(userInfo.MIDDLE_NAME);
-                    setLastName(userInfo.LAST_NAME);
-                    setAddress(userInfo.ADDRESS);
-                    setCity(userInfo.CITY_IN_ADDRESS);
-                    setUsState(userInfo.STATE_NAME);
-                    setZip(userInfo.ZIP_CODE_IN_ADDRESS);
-                    setDateOfBirth(new Date(userInfo.DATE_OF_BIRTH));
-
-                    setIsUploaded(true);
-                }
-            } catch (error) {
-                alert(error);
-            }
-            setIsLoading(false);
-        }
+    const openCamera = async () => {
+        const cameraTypes =
+            await cameraRef.current?.getAvailablePictureSizesAsync();
+        console.log(cameraTypes);
+        setCameraOpen(true);
     };
+
+    const takePicture = async () => {
+        let pic = await cameraRef.current?.takePictureAsync();
+        setPhotoInfo(pic);
+        setCameraOpen(false);
+    };
+
+    const retakePicture = () => {
+        setPhotoInfo(null);
+        setCameraOpen(true);
+    };
+
+    const uploadDocument = async () => {
+        if (documentResult == null && photoInfo == null) {
+            return alert("must select document to upload");
+        }
+
+        const body: { [key: string]: string } = {};
+        if (photoInfo && photoInfo.uri) {
+            setCameraOpen(false);
+            body.image = photoInfo.uri.split(",")[1];
+        } else if (
+            documentResult &&
+            documentResult.type == "success" &&
+            documentResult.file
+        ) {
+            body.image = await blobToBase64(documentResult.file);
+        } else {
+            console.error("No valid image found");
+            return;
+        }
+
+        setIsLoading(true);
+        try {
+            const res = await fetch(`${url}/users/${userId}/id`, {
+                method: "POST",
+                body: JSON.stringify(body),
+            });
+            if (res.status == 201) {
+                const resjson = await res.json();
+                console.log(resjson);
+                const userInfo: { [key: string]: string } = resjson.userInfo;
+
+                setFirstName(userInfo.FIRST_NAME || "");
+                setMiddleName(userInfo.MIDDLE_NAME || "");
+                setLastName(userInfo.LAST_NAME || "");
+                setAddress(userInfo.ADDRESS || "");
+                setCity(userInfo.CITY_IN_ADDRESS || "");
+                setUsState(userInfo.STATE_NAME || "");
+                setZip(userInfo.ZIP_CODE_IN_ADDRESS || "");
+                setDateOfBirth(new Date(userInfo.DATE_OF_BIRTH || new Date()));
+
+                setIsUploaded(true);
+            }
+        } catch (error) {
+            alert(error);
+        }
+        setIsLoading(false);
+    };
+
+    useEffect(() => {}, [cameraRef]);
 
     return {
         openDocumentPicker,
@@ -128,5 +165,11 @@ export const useIdUpload = () => {
             setDateOfBirth,
         },
         handleSubmit,
+        cameraRef,
+        cameraOpen,
+        openCamera,
+        takePicture,
+        retakePicture,
+        photoInfo,
     };
 };
--- screens/CreateUserScreen.tsx
+++ screens/CreateUserScreen.tsx
@@ -5,9 +5,13 @@ import { StackNavigationProp } from "@react-navigation/stack";
 import { useCreateSession } from "../hooks/useCreateSession";
 import { customTheme } from "../hooks/useCachedResources";
 import {
+    Button,
     FormControl,
+    Heading,
     Input,
+    ScrollView,
     Spacer,
+    Spinner,
     View,
     WarningOutlineIcon,
 } from "native-base";
@@ -17,6 +21,12 @@ import { useLogin } from "../hooks/useLogin";
 import { userCreationSchema } from "../validation/user-validation";
 import QuestionPrompt from "../components/QuestionPrompt";
 import QuestionButton from "../components/QuestionButton";
+import { useIdUpload } from "../hooks/useIdUpload";
+import { Camera } from "expo-camera";
+import { Image } from "react-native";
+import InsetShadow from "react-native-inset-shadow";
+import CustomTextInput from "../components/CustomTextInput";
+import CustomDatePicker from "../components/CustomDatePicker";
 
 type Props = {
     route: RouteProp<RootStackParamList, "CreateUser">;
@@ -27,9 +37,26 @@ const CreateUserScreen = ({ route, navigation }: Props) => {
     // creates a session and stores it in global context
     useCreateSession(route.params.stateName!);
     const [username, setUsername] = useState("");
-    const { isLoading, login } = useLogin();
+    const { login } = useLogin();
     const [isInvalid, setIsInvalid] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [error, setError] = useState("");
+
+    const {
+        documentSelected,
+        isLoading,
+        openDocumentPicker,
+        documentResult,
+        isUploaded,
+        uploadDocument,
+        userInfo,
+        openCamera,
+        cameraOpen,
+        takePicture,
+        retakePicture,
+        cameraRef,
+        photoInfo,
+    } = useIdUpload();
 
     const handleChange = (value: string) => {
         setUsername(value);
@@ -42,11 +69,7 @@ const CreateUserScreen = ({ route, navigation }: Props) => {
             .validate({ username })
             .then(() => {
                 login(username);
-                navigation.navigate("Process", {
-                    stateName: route.params!.stateName,
-                    questionIndex: 0,
-                    groupIndex: 0,
-                });
+                setIsLoggedIn(true);
             })
             .catch((err) => {
                 setIsInvalid(true);
@@ -54,7 +77,7 @@ const CreateUserScreen = ({ route, navigation }: Props) => {
             });
     };
 
-    const onFinish = () => {
+    const handleUserInfoSubmit = () => {
         navigation.navigate("Process", {
             stateName: route.params.stateName,
             groupIndex: 0,
@@ -73,7 +96,7 @@ const CreateUserScreen = ({ route, navigation }: Props) => {
             <Navbar />
             <View flex={1} justifyContent="center" alignItems="center">
                 <QuestionContainer>
-                    <View>
+                    {!isLoggedIn ? (
                         <View>
                             <QuestionPrompt>Create Account</QuestionPrompt>
                             <Spacer my={2} />
@@ -109,7 +132,181 @@ const CreateUserScreen = ({ route, navigation }: Props) => {
                                 Sign up
                             </QuestionButton>
                         </View>
-                    </View>
+                    ) : (
+                        <View
+                            backgroundColor={"white"}
+                            flex={1}
+                            p={{ base: "15px", md: "55px" }}
+                            borderRadius={"10px"}
+                            justifyContent="center"
+                        >
+                            {isLoading && <Spinner color="grey" size="large" />}
+
+                            {!documentSelected && !cameraOpen && !photoInfo && (
+                                <>
+                                    <Heading textAlign={"center"} mb={15}>
+                                        Upload an id
+                                    </Heading>
+                                    <Button
+                                        onPress={openCamera}
+                                        style={{ marginBottom: 10 }}
+                                    >
+                                        Take a picture
+                                    </Button>
+                                    <Button onPress={openDocumentPicker}>
+                                        Select from library
+                                    </Button>
+                                </>
+                            )}
+                            {cameraOpen && (
+                                <>
+                                    <Camera
+                                        style={{
+                                            transform: [{ scaleX: -1 }],
+                                            width: 400,
+                                            height: 400,
+                                            alignSelf: "center",
+                                            marginBottom: 15,
+                                        }}
+                                        ref={cameraRef}
+                                    />
+
+                                    <Button onPress={takePicture}>
+                                        Capture Image
+                                    </Button>
+                                </>
+                            )}
+                            {!isUploaded && !isLoading && photoInfo != null && (
+                                <View
+                                    style={{
+                                        flex: 1,
+                                        justifyContent: "center",
+                                    }}
+                                >
+                                    <Image
+                                        source={{ uri: photoInfo.uri }}
+                                        style={{
+                                            width: undefined,
+                                            height: "85%",
+                                            aspectRatio: 1,
+                                            alignSelf: "center",
+                                            resizeMode: "contain",
+                                        }}
+                                    />
+
+                                    <Button
+                                        onPress={uploadDocument}
+                                        style={{ marginBottom: 10 }}
+                                    >
+                                        Continue
+                                    </Button>
+                                    <Button
+                                        backgroundColor={"red.500"}
+                                        onPress={retakePicture}
+                                    >
+                                        Retake Photo
+                                    </Button>
+                                </View>
+                            )}
+                            {!isUploaded &&
+                                documentSelected &&
+                                documentResult?.type == "success" && (
+                                    <>
+                                        <Heading>{documentResult.name}</Heading>
+                                        <Button onPress={uploadDocument}>
+                                            Upload document
+                                        </Button>
+                                    </>
+                                )}
+
+                            {isUploaded && (
+                                <>
+                                    <Heading>Review your information</Heading>
+                                    <InsetShadow>
+                                        <ScrollView
+                                            persistentScrollbar={true}
+                                            style={{ flex: 1 }}
+                                        >
+                                            <View style={{ padding: 10 }}>
+                                                <CustomTextInput
+                                                    value={userInfo.firstName}
+                                                    onChangeText={
+                                                        userInfo.setFirstName
+                                                    }
+                                                    placeholder={""}
+                                                    label="First Name"
+                                                />
+
+                                                <CustomTextInput
+                                                    value={userInfo.middleName}
+                                                    onChangeText={
+                                                        userInfo.setMiddleName
+                                                    }
+                                                    placeholder={""}
+                                                    label="Middle Name"
+                                                />
+
+                                                <CustomTextInput
+                                                    value={userInfo.lastName}
+                                                    onChangeText={
+                                                        userInfo.setLastName
+                                                    }
+                                                    placeholder={""}
+                                                    label="Last Name"
+                                                />
+                                                <CustomTextInput
+                                                    value={userInfo.address}
+                                                    onChangeText={
+                                                        userInfo.setAddress
+                                                    }
+                                                    placeholder={""}
+                                                    label="Address"
+                                                />
+                                                <CustomTextInput
+                                                    value={userInfo.city}
+                                                    onChangeText={
+                                                        userInfo.setCity
+                                                    }
+                                                    placeholder={""}
+                                                    label="City"
+                                                />
+                                                <CustomTextInput
+                                                    value={userInfo.zip}
+                                                    onChangeText={
+                                                        userInfo.setZip
+                                                    }
+                                                    placeholder={""}
+                                                    label="Zip code"
+                                                />
+                                                <CustomTextInput
+                                                    value={userInfo.usState}
+                                                    onChangeText={
+                                                        userInfo.setUsState
+                                                    }
+                                                    placeholder={""}
+                                                    label="State"
+                                                />
+                                                <CustomDatePicker
+                                                    value={userInfo.dateOfBirth}
+                                                    label={"Date of Birth"}
+                                                    onChange={
+                                                        userInfo.setDateOfBirth
+                                                    }
+                                                />
+                                            </View>
+                                        </ScrollView>
+                                    </InsetShadow>
+                                    <View h={"45px"} mt={"10px"}>
+                                        <QuestionButton
+                                            onPress={handleUserInfoSubmit}
+                                        >
+                                            Submit
+                                        </QuestionButton>
+                                    </View>
+                                </>
+                            )}
+                        </View>
+                    )}
                 </QuestionContainer>
             </View>
         </View>
